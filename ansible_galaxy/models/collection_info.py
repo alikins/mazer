03505543bc0d7f703d2918ce0ba88d263b5c02bc
Adrian Likins <alikins@redhat.com>
2018-11-08 19:19:41 -0500

    wip, exp with loading license file once
    instead of everytime we creating a CollectionInfo
    
    We dont need even that, we could 'compile' the data
    into a module with a dict of just id and deprecated
    
    or better... just let the server do it.

--- ansible_galaxy/models/collection_info.py
+++ ansible_galaxy/models/collection_info.py
@@ -1,10 +1,12 @@
 from __future__ import print_function
 
-import attr
 import json
 import logging
 import os
 import re
+
+import prettyprinter
+import attr
 import semver
 
 log = logging.getLogger(__name__)
@@ -12,6 +14,22 @@ log = logging.getLogger(__name__)
 TAG_REGEXP = re.compile('^[a-z0-9]+$')
 
 # see https://github.com/ansible/galaxy/issues/957
+
+
+def load_spdx_licenses():
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    license_path = os.path.join(cwd, '..', 'data', 'spdx_licenses.json')
+
+    log.info('loading %s as json', license_path)
+    licenses = {}
+
+    license_data = json.load(open(license_path, 'r'))
+    for lic in license_data['licenses']:
+        lid = lic['licenseId']
+        licenses[lid] = {'deprecated': lic['isDeprecatedLicenseId']}
+    return licenses
+
+licenses = load_spdx_licenses()
 
 
 @attr.s(frozen=True)
@@ -27,6 +45,14 @@ class CollectionInfo(object):
     # Note galaxy.yml 'dependencies' field is what mazer and ansible
     # consider 'requirements'. ie, install time requirements.
     dependencies = attr.ib(default=[])
+
+    # log.info('valid: %s', valid)
+    # log.info('deprecated: %s', deprecated)
+    prettyprinter.pprint(licenses)
+
+    @property
+    def licenses(self):
+        return licenses
 
     @property
     def namespace(self):
@@ -45,7 +71,7 @@ class CollectionInfo(object):
             self.value_error("'%s' is required" % attribute.name)
 
     @version.validator
-    def _check_version_format(self, attribute, value):
+    def _check_version_format(self, _attribute, value):
         try:
             semver.parse_version_info(value)
         except ValueError:
@@ -53,18 +79,14 @@ class CollectionInfo(object):
                              "instead found '%s'." % value)
 
     @license.validator
-    def _check_license(self, attribute, value):
-        cwd = os.path.dirname(os.path.abspath(__file__))
-        license_path = os.path.join(cwd, '..', 'data', 'spdx_licenses.json')
-        license_data = json.load(open(license_path, 'r'))
-        for lic in license_data['licenses']:
-            if lic['licenseId'] == value:
-                if lic['isDeprecatedLicenseId']:
-                    print("Warning: collection metadata 'license' value '%s' is "
-                          "deprecated." % value)
-                return True
-        self.value_error("Expecting 'license' to be a valid SPDX license ID, instead found '%s'. "
-                         "For more info, visit https://spdx.org" % value)
+    def _check_license(self, _attribute, value):
+        valid = self.licenses.get(value, {})
+        if not valid:
+            self.value_error("Expecting 'license' to be a valid SPDX license ID, instead found '%s'. "
+                             "For more info, visit https://spdx.org" % value)
+
+        if valid.get('deprecated', False):
+            print('License %s is deprecated' % value)
 
     @authors.validator
     @keywords.validator
@@ -74,14 +96,14 @@ class CollectionInfo(object):
             self.value_error("Expecting '%s' to be a list" % attribute.name)
 
     @keywords.validator
-    def _check_keywords(self, attribute, value):
+    def _check_keywords(self, _attribute, value):
         for k in value:
             if not re.match(TAG_REGEXP, k):
                 self.value_error("Expecting keywords to contain alphanumeric characters only, "
                                  "instead found '%s'." % k)
 
     @name.validator
-    def _check_name(self, attribute, value):
+    def _check_name(self, _attribute, value):
         if len(value.split('.', 1)) != 2:
             self.value_error("Expecting 'name' to be in Galaxy name format, <namespace>.<collection_name>, "
                              "instead found '%s'." % value)
