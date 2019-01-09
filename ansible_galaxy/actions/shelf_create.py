71dd09fd940bfb2d998e0dd8009b268cf46865f8
Adrian Likins <alikins@redhat.com>
2019-01-09 12:46:27 -0500

    wip, Find Repositories in collections_path for shelfcreate

--- ansible_galaxy/actions/shelf_create.py
+++ ansible_galaxy/actions/shelf_create.py
@@ -1,8 +1,11 @@
 import logging
 import os
 
-from ansible_galaxy.build import Build, BuildStatuses
-from ansible_galaxy import collection_info
+# from ansible_galaxy.build import Build, BuildStatuses
+# from ansible_galaxy import collection_info
+from ansible_galaxy import installed_repository_db
+from ansible_galaxy import matchers
+# from ansible_galaxy.models.repository_spec import RepositorySpec
 
 log = logging.getLogger(__name__)
 
@@ -15,62 +18,9 @@ def ensure_output_dir(output_path):
     return output_path
 
 
-def _build(galaxy_context,
-           build_context,
-           display_callback=None):
-
-    results = {}
-
-    log.debug('build_context: %s', build_context)
-
-    collection_path = build_context.collection_path
-    collection_info_file_path = os.path.join(collection_path, collection_info.COLLECTION_INFO_FILENAME)
-
-    results['collection_path'] = collection_path
-    results['info_file_path'] = collection_info_file_path
-    results['errors'] = []
-
-    info = None
-
-    try:
-        with open(collection_info_file_path, 'r') as info_fd:
-            info = collection_info.load(info_fd)
-
-            log.debug('info: %s', info)
-    except IOError as e:
-        log.error('Error loading the %s at %s: %s', collection_info.COLLECTION_INFO_FILENAME, collection_info_file_path, e)
-        results['errors'].append('Error loading the %s at %s: %s' % (collection_info.COLLECTION_INFO_FILENAME,
-                                                                     collection_info_file_path, e))
-
-    if not info:
-        results['success'] = False
-        results['errors'].append('There was no collection info in %s' % collection_info_file_path)
-        return results
-
-    builder = Build(build_context=build_context,
-                    collection_info=info)
-
-    ensure_output_dir(build_context.output_path)
-
-    build_results = builder.run(display_callback=display_callback)
-
-    log.debug('build_results: %s', build_results)
-
-    # results here include the builder results and... ?
-    results['build_results'] = build_results
-
-    log.debug('build action results: %s', results)
-
-    if build_results.status == BuildStatuses.success:
-        results['success'] = True
-        return results
-
-    results['success'] = False
-    return results
-
-
 def _create(galaxy_context,
             shelf_creation_context,
+            repository_match_filter=None,
             display_callback=None):
 
     results = {}
@@ -85,12 +35,21 @@ def _create(galaxy_context,
 
     ensure_output_dir(shelf_creation_context.shelf_output_path)
 
-    # find/load collections info from collections_path
-    #  - using InstalledRepositoryDB and or loader helpers (TODO: rename InstalledRepositoryDB)
     # create a ShelfIndex, ShelfRootIndex, ShelfCollectionsIndex, ShelfOtherIndex etc
+    # find/load collections info from collections_path
+    #     add items to ShelfIndex and friends
     # do any file bookkeeping (making backups, or versioning, or scm hook, etc)
     # serialize ShelfIndex etc to shelf_output_path
     # populate any results info needed (date, version, chksums, warnings/errors, etc)
+
+    # if we want to filter out what to include in the shelf, pass in a matcher
+    #  - using InstalledRepositoryDB and or loader helpers (TODO: rename InstalledRepositoryDB)
+    all_repository_match = repository_match_filter or matchers.MatchAll()
+
+    irdb = installed_repository_db.InstalledRepositoryDatabase(galaxy_context)
+
+    for candidate_repository in irdb.select(repository_match_filter=all_repository_match):
+        log.debug('candidate_repo: %s', candidate_repository)
 
     results['create_results'] = {'placeholder': 'nothing to see here yet'}
     results['success'] = True
