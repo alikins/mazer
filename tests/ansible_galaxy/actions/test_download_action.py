55e44703136eb7f46e58cfe170dc3e1b1db93ca2
Adrian Likins <alikins@redhat.com>
2019-05-23 10:22:18 -0400

    Add download action unit tests

---
+++ tests/ansible_galaxy/actions/test_download_action.py
@@ -0,0 +1,61 @@
+import logging
+import os
+
+import pytest
+
+from ansible_galaxy.actions import download
+
+log = logging.getLogger(__name__)
+
+
+def logging_display_callback(msg, **kwargs):
+    log.debug(msg)
+
+
+@pytest.fixture
+def display_callback(mocker):
+    mock_display_callback = mocker.MagicMock(wraps=logging_display_callback)
+    return mock_display_callback
+
+
+def test_download_empty(galaxy_context, display_callback):
+    res = download.download(galaxy_context,
+                            [],
+                            display_callback=display_callback)
+
+    log.debug('res: %s', res)
+    log.debug('display_callback.call_args_list: %s', display_callback.call_args_list)
+
+    assert isinstance(res, dict)
+
+
+def test_run(galaxy_context, mocker, display_callback):
+    mocker.patch('ansible_galaxy.actions.download.download',
+                 return_value={'errors': ['alright I guess'],
+                               'success': True})
+
+    res = download.run(galaxy_context,
+                       requirement_spec_strings=[],
+                       display_callback=display_callback)
+
+    log.debug('res: %s', res)
+    log.debug('display_callback.call_args_list: %s', display_callback.call_args_list)
+
+    assert res == os.EX_OK  # 0
+    assert mocker.call('alright I guess') in display_callback.call_args_list
+
+
+def test_run_error(galaxy_context, mocker, display_callback):
+    mocker.patch('ansible_galaxy.actions.download.download',
+                 return_value={'errors': ['bad!'],
+                               'success': False})
+
+    res = download.run(galaxy_context,
+                       requirement_spec_strings=[],
+                       display_callback=display_callback)
+
+    log.debug('res: %s', res)
+    log.debug('display_callback.call_args_list: %s', display_callback.call_args_list)
+
+    assert res == os.EX_SOFTWARE
+    assert mocker.call('bad!') in display_callback.call_args_list
