6d6a2be0a2a7421a4d061c6282b45c7b343668db
Adrian Likins <alikins@redhat.com>
2019-05-23 15:44:09 -0400

    Implement 'download' subcommand
    Notes: Have to lightly guess at the artifact filename
    since the download url can be a cdn.
    
    default to downloading to cwd if no --dest-path is provided
    
    At the moment, no dep solving implemented.

--- ansible_galaxy/actions/download.py
+++ ansible_galaxy/actions/download.py
@@ -1,16 +1,29 @@
 import logging
 import os
+import shutil
 
+from ansible_galaxy import build  # For the artifact filename templates
 from ansible_galaxy import exceptions
 from ansible_galaxy import requirements
+from ansible_galaxy import repository_spec
 from ansible_galaxy.fetch import fetch_factory
 
 log = logging.getLogger(__name__)
 
 
-def download_collection(galaxy_context,
-                        requirement_to_install,
-                        display_callback=None):
+def artifact_filename(repository_spec):
+    artifact_filename = \
+        build.ARCHIVE_FILENAME_TEMPLATE.format(namespace=repository_spec.namespace,
+                                               name=repository_spec.name,
+                                               version=repository_spec.version,
+                                               extension=build.ARCHIVE_FILENAME_EXTENSION)
+    return artifact_filename
+
+
+def download_requirement(galaxy_context,
+                         requirement_to_install,
+                         full_destination_path,
+                         display_callback=None):
 
     requirement_spec_to_install = requirement_to_install.requirement_spec
 
@@ -20,6 +33,7 @@ def download_collection(galaxy_context,
     # Do a find/check first. We could skip straight to fetch, but in theory, server side
     # can do aliases and substitutions
     try:
+        # Note: This can mutate fetch.requirement_spec as a side effect
         find_results = fetcher.find()
     except exceptions.GalaxyError as e:
         log.debug('requirement_to_install %s failed to be met: %s', requirement_to_install, e)
@@ -27,9 +41,51 @@ def download_collection(galaxy_context,
 
         raise
 
+    repository_spec_to_install = \
+        repository_spec.repository_spec_from_find_results(find_results,
+                                                          requirement_spec_to_install)
+
+    log.debug('repository_spec_to_install: %s', repository_spec_to_install)
+
+    import pprint
+    log.debug('find_results:\n%s', pprint.pformat(find_results))
+
+    artifact_basename = artifact_filename(repository_spec_to_install)
+    log.debug('artifact_basename: %s', artifact_basename)
+
+    try:
+        fetch_results = fetcher.fetch(find_results=find_results)
+
+        log.debug('fetch_results: %s', fetch_results)
+
+        # fetch_results will include a 'archive_path' pointing to where the artifact
+        # was saved to locally.
+    except exceptions.GalaxyError as e:
+        # fetch error probably should just go to a FAILED state, at least until
+        # we have to implement retries
+
+        log.warning('Unable to fetch %s: %s',
+                    repository_spec_to_install.name,
+                    e)
+
+        raise
+
+    tmp_artifact_filename = fetch_results['archive_path']
+
+    full_artifact_dest_filename = os.path.join(full_destination_path, artifact_basename)
+
+    res = shutil.move(tmp_artifact_filename, full_artifact_dest_filename)
+    log.debug('move res: %s', res)
+
+    display_callback('Downloaded %s to %s' % (repository_spec_to_install, full_artifact_dest_filename))
+    # copy/mv the tmp file to dest-dir/artifact_basename
+
+    return full_artifact_dest_filename
+
 
 def download(galaxy_context,
              requirements_list,
+             full_destination_path,
              display_callback=None):
     """API equilivent of 'mazer download' command"""
 
@@ -37,12 +93,20 @@ def download(galaxy_context,
                'success': True,
                }
 
+    for requirement in requirements_list:
+        res = download_requirement(galaxy_context,
+                                   requirement,
+                                   full_destination_path,
+                                   display_callback=display_callback)
+        log.debug('res: %s', res)
+
     return results
 
 
 def run(galaxy_context,
         requirement_spec_strings=None,
         requirement_specs_file=None,
+        destination_path=None,
         display_callback=None):
     """Emulate running 'mazer download' from cli.
 
@@ -62,8 +126,12 @@ def run(galaxy_context,
         # TODO: implement req file
         pass
 
+    real_destination_path = destination_path or os.getcwd()
+    full_destination_path = os.path.abspath(os.path.expanduser(real_destination_path))
+
     results = download(galaxy_context,
                        requirements_list,
+                       full_destination_path,
                        display_callback=display_callback)
 
     if results['errors']:
--- tests/ansible_galaxy/actions/test_download_action.py
+++ tests/ansible_galaxy/actions/test_download_action.py
@@ -18,9 +18,11 @@ def display_callback(mocker):
     return mock_display_callback
 
 
-def test_download_empty(galaxy_context, display_callback):
+def test_download_empty(galaxy_context, display_callback, tmpdir):
+    temp_dir = tmpdir.mkdir('mazer_download_action_unit_test')
     res = download.download(galaxy_context,
                             [],
+                            full_destination_path=temp_dir.strpath,
                             display_callback=display_callback)
 
     log.debug('res: %s', res)
@@ -29,13 +31,29 @@ def test_download_empty(galaxy_context, display_callback):
     assert isinstance(res, dict)
 
 
-def test_run(galaxy_context, mocker, display_callback):
+def test_run_empty(galaxy_context, mocker, display_callback):
     mocker.patch('ansible_galaxy.actions.download.download',
                  return_value={'errors': ['alright I guess'],
                                'success': True})
 
     res = download.run(galaxy_context,
                        requirement_spec_strings=[],
+                       display_callback=display_callback)
+
+    log.debug('res: %s', res)
+    log.debug('display_callback.call_args_list: %s', display_callback.call_args_list)
+
+    assert res == os.EX_OK  # 0
+    assert mocker.call('alright I guess') in display_callback.call_args_list
+
+
+def test_run(galaxy_context, mocker, display_callback):
+    mocker.patch('ansible_galaxy.actions.download.download',
+                 return_value={'errors': ['alright I guess'],
+                               'success': True})
+
+    res = download.run(galaxy_context,
+                       requirement_spec_strings=['alikins.whatever'],
                        display_callback=display_callback)
 
     log.debug('res: %s', res)
