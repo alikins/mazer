f8fd4a189c7b8a32878a5d8de843396cb8cf1629
Adrian Likins <alikins@redhat.com>
2019-05-23 13:58:46 -0400

    Start implementing download only

--- ansible_galaxy/actions/download.py
+++ ansible_galaxy/actions/download.py
@@ -1,7 +1,31 @@
 import logging
 import os
 
+from ansible_galaxy import exceptions
+from ansible_galaxy import requirements
+from ansible_galaxy.fetch import fetch_factory
+
 log = logging.getLogger(__name__)
+
+
+def download_collection(galaxy_context,
+                        requirement_to_install,
+                        display_callback=None):
+
+    requirement_spec_to_install = requirement_to_install.requirement_spec
+
+    fetcher = fetch_factory.get(galaxy_context=galaxy_context,
+                                requirement_spec=requirement_spec_to_install)
+
+    # Do a find/check first. We could skip straight to fetch, but in theory, server side
+    # can do aliases and substitutions
+    try:
+        find_results = fetcher.find()
+    except exceptions.GalaxyError as e:
+        log.debug('requirement_to_install %s failed to be met: %s', requirement_to_install, e)
+        log.warning('Unable to find metadata for %s: %s', requirement_spec_to_install.label, e)
+
+        raise
 
 
 def download(galaxy_context,
@@ -29,6 +53,14 @@ def run(galaxy_context,
     # TODO: convert requirement_spec_strings and/or requirement_specs_file into
     #       a requirements_list
     requirements_list = []
+
+    if requirement_spec_strings:
+        requirements_list += \
+            requirements.requirements_from_strings(repository_spec_strings=requirement_spec_strings)
+
+    if requirement_specs_file:
+        # TODO: implement req file
+        pass
 
     results = download(galaxy_context,
                        requirements_list,
