a1159d060c7f5595af5bfe9aec172bf0391af732
Adrian Likins <alikins@redhat.com>
2019-05-26 17:18:45 -0400

    Update local_file fetch for local installs work again

--- ansible_galaxy/fetch/local_file.py
+++ ansible_galaxy/fetch/local_file.py
@@ -5,7 +5,9 @@ import os
 from ansible_galaxy import collection_artifact
 from ansible_galaxy import repository
 from ansible_galaxy import repository_archive
+from ansible_galaxy import repository_spec
 from ansible_galaxy.fetch import base
+from ansible_galaxy.models.repository_spec import RepositorySpec
 
 log = logging.getLogger(__name__)
 
@@ -19,16 +21,34 @@ class LocalFileFetch(base.BaseFetch):
         # update local_path to point to the artifact
         self.local_path = requirement_spec.src
 
-        results = {'content': {'galaxy_namespace': requirement_spec.namespace,
-                               'repo_name': requirement_spec.name,
-                               # For a local file, we created the version_spec to match
-                               # exactly the version from the file, so dig into the version_spec
-                               # a bit to pull that out.
-                               # TODO/FIXME: helper method/wrapper for making this less coupled
-                               'version': str(vspec)},
+        # 'resolved_namespace' and 'resolved_name' included here if different
+        repo_spec_germ = {'galaxy_namespace': requirement_spec.namespace,
+                          'repo_name': requirement_spec.name,
+                          # For a local file, we created the version_spec to match
+                          # exactly the version from the file, so dig into the version_spec
+                          # a bit to pull that out.
+                          # TODO/FIXME: helper method/wrapper for making this less coupled
+                          'version': str(vspec)}
+
+        repo_spec_data = \
+            repository_spec.repository_spec_data_from_find_results({'content': repo_spec_germ},
+                                                                   requirement_spec)
+
+        repository_spec_to_install = RepositorySpec.from_dict(repo_spec_data)
+
+        # TODO: have to dig into the local file to find it's dependencies
+        repo = self._load(self.local_path)
+
+        log.debug('repo.requirements: %s', repo.requirements)
+
+        # the name from the metadata in the artifact
+        repo_spec_germ['fetched_name'] = repo.repository_spec.name
+
+        results = {'content': repo_spec_germ,
+                   'repository_spec_to_install': repository_spec_to_install,
+                   'requirements': repo.requirements,
                    'artifact': {'filename': os.path.basename(self.local_path),
                                 'size': os.path.getsize(self.local_path)},
-                   'custom': {},
                    }
 
         return results
@@ -53,9 +73,11 @@ class LocalFileFetch(base.BaseFetch):
         results = {'archive_path': repository_archive_path,
                    'fetch_method': self.fetch_method}
 
+        # repo = self._load(repository_archive_path)
+
         results['custom'] = {'local_path': self.local_path}
         results['content'] = find_results['content']
-        results['content']['fetched_name'] = repo.repository_spec.name
+        # results['content']['fetched_name'] = repo.repository_spec.name
         # This will have filename, size but usually not sha256
         results['artifact'] = find_results['artifact']
 
@@ -70,6 +92,16 @@ class LocalFileFetch(base.BaseFetch):
     def _load_repository(self, repository_archive):
         repo = repository.load_from_archive(repository_archive)
         log.debug('repo: %s', repo)
+        return repo
+
+    def _load(self, archive_path):
+
+        log.debug('repository_archive_path=%s (inplace)', archive_path)
+
+        repo_archive = self._load_repository_archive(archive_path)
+
+        repo = self._load_repository(repo_archive)
+
         return repo
 
     def cleanup(self):
